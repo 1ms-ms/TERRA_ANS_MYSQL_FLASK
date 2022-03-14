b6b5836149684dc33b91aa399c4af4adcd0636f4
ms <vomircom@gmail.com>
2022-03-14 19:36:03 +0100

    Create securitygroup.tf

---
+++ IaC/securitygroup.tf
@@ -0,0 +1,65 @@
+resource "aws_security_group" "sg_PUB" {
+  name        = "SG_TERRAFORM"
+  vpc_id      = aws_vpc.vpc_PUB.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "HTTPs"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG_TERRAFORM_PUB"
+  }
+}
+resource "aws_security_group" "sg_PRIV" {
+  name        = "SG_TERRAFORM"
+  vpc_id      = aws_vpc.vpc_PRIV.id
+  
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "MYSQL"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["10.10.0.0/16"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG_TERRAFORM_PRIV"
+  }
+}
