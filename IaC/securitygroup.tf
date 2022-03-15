cd569c71f67c016327c985cfe0d70e7bf8c11f35
ms <vomircom@gmail.com>
2022-03-15 18:34:12 +0100

    Update securitygroup.tf

--- IaC/securitygroup.tf
+++ IaC/securitygroup.tf
@@ -1,42 +1,6 @@
-resource "aws_security_group" "sg_PUB" {
-  name        = "SG_TERRAFORM"
-  vpc_id      = aws_vpc.vpc_PUB.id
-
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  ingress {
-    description      = "HTTPs"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "SG_TERRAFORM_PUB"
-  }
-}
-resource "aws_security_group" "sg_PRIV" {
-  name        = "SG_TERRAFORM"
-  vpc_id      = aws_vpc.vpc_PRIV.id
+resource "aws_security_group" "sg_app" {
+  name        = "SG_TERRAFORM_app"
+  vpc_id      = aws_vpc.vpc.id
   
   ingress {
     description      = "SSH"
@@ -46,11 +10,18 @@ resource "aws_security_group" "sg_PRIV" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
   ingress {
-    description      = "MYSQL"
-    from_port        = 3306
-    to_port          = 3306
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["10.10.0.0/16"]
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "Flask"
+    from_port        = 5000
+    to_port          = 5000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
   egress {
     from_port        = 0
@@ -60,6 +31,42 @@ resource "aws_security_group" "sg_PRIV" {
   }
 
   tags = {
-    Name = "SG_TERRAFORM_PRIV"
+    Name = "SG_TERRAFORM_app"
+  }
+}
+resource "aws_security_group" "sg_db" {
+  name        = "SG_TERRAFORM_db"
+  vpc_id      = aws_vpc.vpc.id
+  
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "DB"
+    from_port        = 3306
+    to_port          = 3306 
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG_TERRAFORM_db"
   }
 }
