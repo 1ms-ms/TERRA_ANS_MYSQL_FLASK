b1c4eb64aba3e47328f5f8c6d743bf4dc814b1b9
ms <vomircom@gmail.com>
2022-03-15 18:33:55 +0100

    Update network_pub.tf

--- IaC/network_pub.tf
+++ IaC/network_pub.tf
@@ -1,13 +1,13 @@
-resource "aws_vpc" "vpc_PUB" {
+resource "aws_vpc" "vpc" {
   cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name        = "Public VPC"
+    Name        = "VPC"
   }
 }
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.vpc_PUB.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
   tags = {
     Name = "GW_TERRAFORM"
   }
@@ -17,28 +17,35 @@ resource "aws_eip" "eip" {
   depends_on = [aws_internet_gateway.gw]
 }
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = "${aws_vpc.vpc_PUB.id}"
-  count                   = 1
-  cidr_block              = "10.10.0.0/24"
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.10.10.0/24"
   availability_zone       = "eu-west-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name        = element(var.sub_PUB, count.index)
+    Name        = element(var.sub_PUB, 1)
   }
 }
 resource "aws_route_table" "rt-public" {
-  vpc_id = "${aws_vpc.vpc_PUB.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
   route {
     cidr_block    = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-  route {
-    cidr_block    = "10.20.0.0/16"
-    vpc_peering_connection_id = aws_vpc_peering_connection.VPC_PEER.id
-  }
   tags = {
     Name = "RT_TERRAFORM_PUBLIC"
   }
+}
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+resource "aws_nat_gateway" "NAT" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+  depends_on = [aws_internet_gateway.gw]
 }
 resource "aws_route_table_association" "RTA-public" {
   count          = 1
